c6b1d41333270b35175abb100f16d738a4b431bc
Lucas Abijmil <lucas.abijmil@gmail.com>
2025-03-21 12:16:16 +0100

    Craete Model package to share model

---
+++ Model/Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "Model",
+  platforms: [.iOS(.v17)],
+  products: [.library(name: "Model", targets: ["Model"])],
+  targets: [.target(name: "Model"), .testTarget(name: "ModelTests", dependencies: ["Model"])]
+)
---
+++ Model/Sources/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Model
+//
+//  Created by Lucas Abijmil on 21/03/2025.
+//
+
+import Foundation
